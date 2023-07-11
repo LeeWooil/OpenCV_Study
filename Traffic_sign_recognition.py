e915ca7e6a8877271bbfaa25fa1758caf1ed1b4d
LeeWooil <lwi2765@khu.ac.kr>
2023-07-11 23:20:51 +0900

    Hyper_Project

--- Traffic_sign_recognition.py
+++ Traffic_sign_recognition.py
@@ -1,34 +1,63 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import pytesseract
+import PIL
+plt.style.use('dark_background')
 
-def houghCircle():
-    # 원이 있는 이미지를 올림.
-    img1 = cv2.imread("C:\Hyper\speed.png")
-    img2 = img1.copy()
+img = cv2.imread("C:\Hyper\OpenCV_Study\speed.png")
+height, width, channel = img.shape
 
-    # 0~9까지 가우시안 필터로 흐리게 만들어 조절함.
-    img2 = cv2.GaussianBlur(img2, (9, 9), 0)
-    # 그레이 이미지로 바꿔서 실행해야함.
-    imgray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
-    # 원본과 비율 / 찾은 원들간의 최소 중심거리 / param1, param2를 조절해 원을 찾음
-    circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 10, param1=60, param2=50, minRadius=0, maxRadius=0)
-
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-
-        print(circles)
-
-        for i in circles[0, :]:
-            cv2.circle(img1, (i[0], i[1]), i[2], (255, 255, 0), 2)
-
-        cv2.imshow('ori',img2)
-        cv2.imshow('HoughCircle', img1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
-        print('원을 찾을 수 없음')
+structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
-houghCircle()
+imgTopHat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, structuringElement)
+imgBlackHat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, structuringElement)
+
+imgGrayscalePlusTopHat = cv2.add(gray, imgTopHat)
+gray = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+img_blurred = cv2.GaussianBlur(gray, ksize=(5, 5), sigmaX=0)
+
+img_thresh = cv2.adaptiveThreshold(
+    img_blurred, 
+    maxValue=255.0, 
+    adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+    thresholdType=cv2.THRESH_BINARY_INV, 
+    blockSize=19, 
+    C=9
+)
+
+contours, _= cv2.findContours(img_thresh,mode=cv2.RETR_TREE,
+                              method=cv2.CHAIN_APPROX_SIMPLE)
+
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+cv2.drawContours(temp_result, contours=contours, contourIdx=-1, 
+                 color=(255, 255, 255))
+
+temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+contours_dict = []
+
+for contour in contours:
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(temp_result, pt1=(x, y), pt2=(x+w, y+h), 
+                  color=(255, 255, 255), thickness=2)
+    
+    
+    contours_dict.append({
+        'contour': contour,
+        'x': x,
+        'y': y,
+        'w': w,
+        'h': h,
+        'cx': x + (w / 2),
+        'cy': y + (h / 2)
+    })
+
+plt.figure(figsize=(12,10))
+cv2.imshow('gray', temp_result)
+cv2.waitKey()
+cv2.destroyAllWindows()
